25e0ee81ba73cf822d5e6cb050d678472d993b14
bgardner-edu <gar19078@byui.edu>
2025-01-15 22:06:59 -0700

    finished tutorial project and started CMS project

---
+++ W02_CourseProject/CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  standalone: false,
+  
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("A test recipe", "This is a test", "")
+  ];
+
+  constructor() {}
+  ngOnInit() {
+
+  }
+}
