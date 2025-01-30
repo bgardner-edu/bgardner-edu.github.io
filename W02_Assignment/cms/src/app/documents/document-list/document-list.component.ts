816b255a74a678699b28bfc2c089251e2ca92dea
bgardner-edu <gar19078@byui.edu>
2025-01-29 21:00:00 -0700

    W04_ Assignment

--- W02_Assignment/cms/src/app/documents/document-list/document-list.component.ts
+++ W02_Assignment/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
   standalone: false,
-  
+
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents:Document[] = [
+    new Document("1", "Name", "description", "url", null),
+    new Document("2", "doc 1", "description", "url", null),
+    new Document("3", "doc 2", "description", "url", null),
+    new Document("4", "doc 3", "description", "url", null),
+    new Document("5", "doc 3", "description", "url", null)
+  ];
 
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+  }
 }
--- W02_Assignment/cms/src/app/documents/documents.component.ts
+++ W02_Assignment/cms/src/app/documents/documents.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './documents.component.css'
 })
 export class DocumentsComponent {
+  selectedDocument: Document;
 
+  onDocumentDetails(document: Document){
+    console.log(document);
+    this.selectedDocument = document;
+  }
 }
